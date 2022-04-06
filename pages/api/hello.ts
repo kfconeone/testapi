add3bf6b2b653a92ebbd359fdd38016cda1f0536
kfconeone <kfconeone@gmail.com>
2022-04-06 13:59:38 +0800

    print the req2

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,7 +9,8 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  console.log("this is the request body");
   console.log(req.body);
 
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name: "John Doe222" });
 }
