0e8fc182e9e5837fc3ef4746af7fa7305bd94d72
kfconeone <kfconeone@gmail.com>
2022-04-06 16:08:13 +0800

    add SAKEY5

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,17 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+
+var admin = require("firebase-admin");
+const serviceAccount = JSON.parse(process.env.SAKEY as string);
+
 type Data = {
   name: string;
 };
 
-export default function handler(
+var db: any = null;
+function getDb() {
+  if (db != null) return db;
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+  return admin.firestore();
+}
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   try {
+    let db = getDb();
+
     console.log("this is the request body");
-    console.log(req.body);
-    console.log(process.env.SAKEY);
+
+    let doc = await db.collection("Users").doc("User001").get();
+    console.log(doc.id);
+    console.log(doc.data());
   } catch (error) {
     console.log("error: ", error);
   }
